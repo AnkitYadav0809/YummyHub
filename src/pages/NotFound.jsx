f3ae43145aaa310de187b2129a1c411e507fefcf
Ankit Yadav <ankityadav60062@gmail.com>
2024-09-05 10:51:51 +0530

    removed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
   return (
     <div className="flex items-center justify-center flex-col mt-10 h-full w-full gap-5 pt-16">
       <img className="w-1/4" src={error404} alt="Error 404 page not found" />
-      <h3 className="text-3xl font-semibold">Oops!! Something went wrong...</h3>
+      <h3 className="text-3xl font-semibold">Oops!! Something  wrong...</h3>
       <h3 className="text-2xl">{status + ": " + statusText}</h3>
       <h3 className="text-2xl">{error.message}</h3>
     </div>
